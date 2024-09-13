efb6f5b2398d10328d0f8877d7b8aa6aeb64675c
lsh1315 <lsh000407@hanmail.net>
2024-09-13 17:54:39 +0900

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,40 +1,15 @@
 import RPi.GPIO as GPIO
-import threading
 import time
-import Adafruit_DHT
-
-buttonPin = 25
-
-ledRed = 17
-ledYello = 27
-ledGreen = 22
 
 sensTouch = 5
-sensDH = 6
-
-DistanceTrig = 23
-DistanceEcho = 24
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ledYello, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ledGreen, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(sensTouch, GPIO.IN)
 GPIO.setup(sensDH, GPIO.IN)
-GPIO.setup(DistanceTrig, GPIO.OUT)
-GPIO.setup(DistanceEcho, GPIO.IN)
-
-flag = 0
-touch = 0
-
-
-
-def mainProcess():
+        
+while True:
     if GPIO.input(sensTouch) == 1:
         print("터치되었습니다")
     else:
         print("터치 되지 않았습니다")
-        
-while True:
-    mainProcess()
+    time.sleep(0.5)
