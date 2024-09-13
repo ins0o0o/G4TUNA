347989536fcb9b8b280592dc2ec21ee58eb9bd38
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-09-13 17:12:43 +0900

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
+import RPi.GPIO as GPIO
+import time
+
+ledRed = 17
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
+
+
+    while True:
+        GPIO.output(ledRed, GPIO.HIGH)  # LED 켜기
+        time.sleep(1)  # 1초 대기
+        GPIO.output(ledRed, GPIO.LOW)   # LED 끄기
+        time.sleep(1)  # 1초 대기
 
