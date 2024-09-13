c9c729b2bd78d9fd0474b1d12109b044c3aa493f
JOOHYEONG-SOHN <sohnjohn01@gmail.com>
2024-09-13 17:52:33 +0900

    Update test4.py

--- test4.py
+++ test4.py
@@ -27,25 +27,14 @@ GPIO.setup(DistanceEcho, GPIO.IN)
 
 flag = 0
 touch = 0
-def check_button():
-    global touch
-    while True:
-        if GPIO.input(sensTouch) == 1:
-            touch = 1
 
-touch_thread = threading.Thread(target=check_button)
-touch_thread.daemon = True  # 메인 스레드가 종료되면 이 스레드도 종료되도록 설정
-touch_thread.start()
 
 
 def mainProcess():
-    global touch
-    distance = measure_distance()
-    if touch == 1:
-        print("터치되었습니다.")
+    if GPIO.input(sensTouch) == 1:
+        print("터치되었습니다")
     else:
-        print("터치 되지 않았습니다.")
-
-
+        print("터치 되지 않았습니다")
+        
 while True:
     mainProcess()
