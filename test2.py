e200c0d2e5a393cbffbbba66accb25e891e2f656
lsh1315 <lsh000407@hanmail.net>
2024-09-13 18:10:12 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,16 +5,16 @@ ledRed = 17
 ledYello = 27
 ledGreen = 22
 
-sensTouch = 5
+buttonPin = 5
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ledYello, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ledGreen, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(sensTouch, GPIO.IN)
+GPIO.setup(buttonPin, GPIO.IN)
 
 while True:
-    if GPIO.input(sensTouch) == 1:
+    if GPIO.input(buttonPin) == 1:
         GPIO.output(ledRed, GPIO.HIGH)
         GPIO.output(ledYello, GPIO.HIGH)
         GPIO.output(ledGreen, GPIO.HIGH)
