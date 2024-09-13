bde12571618a1b92327721cb0a45803a7f457508
JOOHYEONG-SOHN <sohnjohn01@gmail.com>
2024-09-13 17:55:24 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,36 +1,25 @@
 import RPi.GPIO as GPIO
-import threading
 import time
-import Adafruit_DHT
 
-buttonPin = 25
+
 
 ledRed = 17
 ledYello = 27
 ledGreen = 22
 
 sensTouch = 5
-sensDH = 6
 
-DistanceTrig = 23
-DistanceEcho = 24
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ledYello, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(ledGreen, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(sensTouch, GPIO.IN)
-GPIO.setup(sensDH, GPIO.IN)
-GPIO.setup(DistanceTrig, GPIO.OUT)
-GPIO.setup(DistanceEcho, GPIO.IN)
 
-flag = 0
-touch = 0
-
-
-
-def mainProcess():
+    
+ 
+while True:
     if GPIO.input(sensTouch) == 1:
         GPIO.output(ledRed, GPIO.HIGH)
         GPIO.output(ledYello, GPIO.HIGH)
@@ -39,7 +28,3 @@ def mainProcess():
         GPIO.output(ledRed, GPIO.LOW)
         GPIO.output(ledYello, GPIO.LOW)
         GPIO.output(ledGreen, GPIO.LOW)
-    
- 
-while True:
-    mainProcess()
