9a3e4a4fe261cccff3e91cd1c1e5c1f294b2a2c4
Lee Seunghyun <lsh000407@hanmail.net>
2024-09-20 20:24:16 +0900

    update

--- week3/test5.py
+++ week3/test5.py
@@ -30,6 +30,22 @@ humidity = 0
 temperature = 0
 
 
+def check_touch():
+    global flag
+    while True:
+        GPIO.wait_for_edge(sensTouch, GPIO.FALLING)  # 버튼이 눌렸을 때까지 대기 (FALLING 엣지 감지)
+        flag = 1 - flag  # flag 값 토글
+        if flag == 1:
+            print("\n<<< Process Start!! >>>\n")
+        else:
+            print("\n<<< Process Terminated >>>\n")
+        time.sleep(0.5)
+
+touch_thread = threading.Thread(target=check_touch)
+touch_thread.daemon = True  # 메인 스레드가 종료되면 이 스레드도 종료되도록 설정
+touch_thread.start()
+
+
 def readDH():
     sensor = Adafruit_DHT.DHT11     # sensor 객체 생성
     global humidity
@@ -37,8 +53,7 @@ def readDH():
     while True:                     #0.5초마다 온도와 습도 값을 불러옴
         humidity, temperature = Adafruit_DHT.read_retry(sensor, sensDH)
         time.sleep(0.5)
-
-thread = threading.Thread(target=readDH)
-thread.daemon = True  # 메인 프로세스 종료 시 자동으로 종료
-thread.start()
-
+        
+DH_thread = threading.Thread(target=readDH)
+DH_thread.daemon = True  # 메인 프로세스 종료 시 자동으로 종료
+DH_thread.start()
