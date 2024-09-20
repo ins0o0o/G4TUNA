87fc5d065ac1ec0234c5de971d4ad0622968f042
Lee Seunghyun <lsh000407@hanmail.net>
2024-09-20 19:56:25 +0900

    wtf

--- week3/test5.py
+++ week3/test5.py
@@ -4,3 +4,22 @@ import threading
 import time
 import Adafruit_DHT
 
+ledRed = 17
+ledYello = 27
+ledGreen = 22
+
+sensTouch = 5
+sensDH = 6
+
+DistanceTrig = 23
+DistanceEcho = 24
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ledYello, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ledGreen, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(sensTouch, GPIO.IN)
+GPIO.setup(sensDH, GPIO.IN)
+GPIO.setup(DistanceTrig, GPIO.OUT)
+GPIO.setup(DistanceEcho, GPIO.IN)
+
