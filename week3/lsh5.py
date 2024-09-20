eb4613e39b864ca2f1f70221839c44274cc72fa9
lsh1315 <lsh000407@hanmail.net>
2024-09-21 02:26:25 +0900

    Update lsh5.py

--- week3/lsh5.py
+++ week3/lsh5.py
@@ -26,8 +26,9 @@ GPIO.setup(DistanceEcho, GPIO.IN)
 button_states = {
     'button1': True,
     'button2': False,
-    'button3': True,
-    'button4': False
+    'button3': False,
+    'button4': True,
+    'button5': False
 }
 
 AutoAccBrk = 0  # 차간 거리 유지 ON / OFF
@@ -54,11 +55,11 @@ def measure_DH():
     global humidity
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(sensor, sensDH)
-        if temperature < 25:
-            button_states['button4'] = True
-            GPIO.output[ledYellow, 1]
-        else:
-            button_states['button4'] = False
+        if button_states['button4'] == True:
+            if temperature < 25:
+                button_states['button5'] = True
+            else:
+                button_states['button5'] = False
         time.sleep(0.2)
 
 DH_thread = threading.Thread(target=measure_DH)
@@ -67,28 +68,17 @@ DH_thread.start()
 
 def measure_distance():
     global distance
-    # Trig 핀을 LOW로 설정하고 짧은 시간 대기
     while True:
         GPIO.output(DistanceTrig, GPIO.LOW)
         time.sleep(0.1)
-        
-        # Trig 핀에 짧은 펄스(10μs) 발생
         GPIO.output(DistanceTrig, GPIO.HIGH)
         time.sleep(0.00001)  # 10μs 대기
         GPIO.output(DistanceTrig, GPIO.LOW)
-        
-        # Echo 핀이 HIGH로 될 때까지 대기
         while GPIO.input(DistanceEcho) == GPIO.LOW:
             pulse_start = time.time()  # Echo 핀이 LOW에서 HIGH로 바뀌는 순간 기록
-        
-        # Echo 핀이 LOW로 될 때까지 대기
         while GPIO.input(DistanceEcho) == GPIO.HIGH:
             pulse_end = time.time()  # Echo 핀이 HIGH에서 LOW로 바뀌는 순간 기록
-        
-        # 펄스 지속 시간 계산
         pulse_duration = pulse_end - pulse_start
-        
-        # 초음파 속도는 34300 cm/s, 따라서 거리 = 시간 * 속도 / 2 (왕복이므로 2로 나눔)
         distance = pulse_duration * 34300 / 2
         
         if button_states['button1'] == True:
@@ -189,7 +179,7 @@ def index():
                     onchange="this.form.submit()">
                     <span class="slider"></span>
                 </label>
-                <span class="label-text">AUTO</span>
+                <span class="label-text">ADAS</span>
             </div>
         </form>
 
@@ -219,6 +209,17 @@ def index():
             <div class="switch-container">
                 <label class="switch">
                     <input type="checkbox" name="button4" {% if button_states['button4'] %}checked{% endif %} 
+                    onchange="this.form.submit()">
+                    <span class="slider"></span>
+                </label>
+                <span class="label-text">Auto Air Conditional</span>
+            </div>
+        </form>
+
+        <form method="POST" action="/toggle_button5">
+            <div class="switch-container">
+                <label class="switch">
+                    <input type="checkbox" name="button5" {% if button_states['button5'] %}checked{% endif %} 
                     onchange="this.form.submit()">
                     <span class="slider"></span>
                 </label>
@@ -261,6 +262,13 @@ def toggle_button4():
     global button_states
     button_states['button1'] = False
     button_states['button4'] = not button_states['button4']
+    return redirect(url_for('index'))
+
+@app.route('/toggle_button4', methods=['POST'])
+def toggle_button5():
+    global button_states
+    button_states['button4'] = False
+    button_states['button5'] = not button_states['button4']
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
