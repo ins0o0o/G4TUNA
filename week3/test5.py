3772835db8a474270cdf7fb8c3e5b09128f163ce
Lee Seunghyun <lsh000407@hanmail.net>
2024-09-20 20:18:29 +0900

    update

--- week3/test5.py
+++ week3/test5.py
@@ -23,3 +23,22 @@ GPIO.setup(sensDH, GPIO.IN)
 GPIO.setup(DistanceTrig, GPIO.OUT)
 GPIO.setup(DistanceEcho, GPIO.IN)
 
+AutoAccBrk = 0  # 차간 거리 유지 ON / OFF
+AutoAC = 0      # 에어컨 자동 ON / OFF
+
+humidity = 0
+temperature = 0
+
+
+def readDH():
+    sensor = Adafruit_DHT.DHT11     # sensor 객체 생성
+    global humidity
+    global temperature
+    while True:                     #0.5초마다 온도와 습도 값을 불러옴
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, sensDH)
+        time.sleep(0.5)
+
+thread = threading.Thread(target=readDH)
+thread.daemon = True  # 메인 프로세스 종료 시 자동으로 종료
+thread.start()
+
