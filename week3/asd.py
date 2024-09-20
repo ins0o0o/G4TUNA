c6eb3764322d59120969cb3dde4db4e5d3a0b0f1
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-09-21 01:05:51 +0900

    Update asd.py

--- week3/asd.py
+++ week3/asd.py
@@ -15,76 +15,84 @@ DistanceTrig = 23
 DistanceEcho = 24
 
 GPIO.setmode(GPIO.BCM)
-ledPin = [ledRed, ledYello, ledGreen]
-ledStates = [0, 0, 0]  # Red, Yellow, Green 순서로 상태 저장
-
-GPIO.setup(ledPin[0], GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ledPin[1], GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(ledPin[2], GPIO.OUT, initial=GPIO.LOW)
-
+GPIO.setup(ledRed, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ledYello, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(ledGreen, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(sensTouch, GPIO.IN)
 GPIO.setup(sensDH, GPIO.IN)
 GPIO.setup(DistanceTrig, GPIO.OUT)
 GPIO.setup(DistanceEcho, GPIO.IN)
 
-AutoAccBrk = 0  # 차간 거리 유지 ON / OFF
-AutoAC = 0      # 에어컨 자동 ON / OFF
+button_states = {
+    'button1': True,
+    'button2': False,
+    'button3': True,
+    'button4': False
+}
 
 humidity = 0
 temperature = 0
 distance = 0
 
+ledStates = [0, 0, 0]  # Red, Yellow, Green LED 상태
+
 # LED 상태 업데이트 함수
 def updateLeds():
-    for num, value in enumerate(ledStates):
-        GPIO.output(ledPin[num], value)
+    GPIO.output(ledRed, ledStates[0])
+    GPIO.output(ledYello, ledStates[1])
+    GPIO.output(ledGreen, ledStates[2])
 
 # 터치 센서 스레드
 def check_touch():
+    global button_states
     while True:
         GPIO.wait_for_edge(sensTouch, GPIO.FALLING)
-        ledStates[0] = not ledStates[0]  # 터치 시 Red LED 토글
-        updateLeds()
+        button_states['button1'] = not button_states['button1']
         time.sleep(0.2)
 
 touch_thread = threading.Thread(target=check_touch)
-touch_thread.daemon = True  # 메인 스레드 종료 시 자동 종료
+touch_thread.daemon = True
 touch_thread.start()
 
 # 거리 측정 스레드
 def measure_distance():
     sensor = Adafruit_DHT.DHT11
+    global button_states
     global distance
     global temperature
     global humidity
-    
     while True:
         humidity, temperature = Adafruit_DHT.read_retry(sensor, sensDH)
 
         GPIO.output(DistanceTrig, GPIO.LOW)
         time.sleep(0.1)
-        
+
         GPIO.output(DistanceTrig, GPIO.HIGH)
         time.sleep(0.00001)
         GPIO.output(DistanceTrig, GPIO.LOW)
-        
+
         while GPIO.input(DistanceEcho) == GPIO.LOW:
             pulse_start = time.time()
-        
+
         while GPIO.input(DistanceEcho) == GPIO.HIGH:
             pulse_end = time.time()
-        
+
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 34300 / 2
 
-        if distance < 10:
-            ledStates[0] = 1  # Red LED ON
-            ledStates[2] = 0  # Green LED OFF
-        elif distance < 30:
-            ledStates[2] = 1  # Green LED ON
-            ledStates[0] = 0  # Red LED OFF
+        if button_states['button1'] == True:
+            if distance < 10:
+                button_states['button2'] = False
+                ledStates[0] = 1  # Red LED ON
+                button_states['button3'] = True
+                ledStates[2] = 0  # Green LED OFF
+            elif distance > 10:
+                button_states['button2'] = True
+                ledStates[0] = 0  # Red LED OFF
+                button_states['button3'] = False
+                ledStates[2] = 1  # Green LED ON
         updateLeds()
-        
+
         time.sleep(0.2)
 
 measure_thread = threading.Thread(target=measure_distance)
@@ -105,23 +113,20 @@ html_page = '''
     <p>거리: {{distance}} cm</p>
     <p>온도: {{temperature}} °C</p>
 
-    <div style="padding-left:20px;">
-        <p>
-            <b>Red LED: {% if ledStates[0]==1 %} ON {% else %} OFF {% endif %}</b> 
-            <a href="{{ url_for('ledswitch', LEDN=0, state=1) }}"><input type="button" value="ON"></a>
-            <a href="{{ url_for('ledswitch', LEDN=0, state=0) }}"><input type="button" value="OFF"></a>
-        </p>
-        <p>
-            <b>Yellow LED: {% if ledStates[1]==1 %} ON {% else %} OFF {% endif %}</b> 
-            <a href="{{ url_for('ledswitch', LEDN=1, state=1) }}"><input type="button" value="ON"></a>
-            <a href="{{ url_for('ledswitch', LEDN=1, state=0) }}"><input type="button" value="OFF"></a>
-        </p>
-        <p>
-            <b>Green LED: {% if ledStates[2]==1 %} ON {% else %} OFF {% endif %}</b> 
-            <a href="{{ url_for('ledswitch', LEDN=2, state=1) }}"><input type="button" value="ON"></a>
-            <a href="{{ url_for('ledswitch', LEDN=2, state=0) }}"><input type="button" value="OFF"></a>
-        </p>
-    </div>
+    <form method="POST" action="/toggle_led/0">
+        <p>Red LED: {% if ledStates[0] == 1 %} ON {% else %} OFF {% endif %} 
+        <input type="submit" value="{% if ledStates[0] == 1 %} Turn OFF {% else %} Turn ON {% endif %}"></p>
+    </form>
+
+    <form method="POST" action="/toggle_led/1">
+        <p>Yellow LED: {% if ledStates[1] == 1 %} ON {% else %} OFF {% endif %} 
+        <input type="submit" value="{% if ledStates[1] == 1 %} Turn OFF {% else %} Turn ON {% endif %}"></p>
+    </form>
+
+    <form method="POST" action="/toggle_led/2">
+        <p>Green LED: {% if ledStates[2] == 1 %} ON {% else %} OFF {% endif %} 
+        <input type="submit" value="{% if ledStates[2] == 1 %} Turn OFF {% else %} Turn ON {% endif %}"></p>
+    </form>
 </body>
 </html>
 '''
@@ -131,10 +136,10 @@ html_page = '''
 def index():
     return render_template_string(html_page, ledStates=ledStates, distance=distance, temperature=temperature)
 
-# LED 스위치 라우트
-@app.route('/<int:LEDN>/<int:state>')
-def ledswitch(LEDN, state):
-    ledStates[LEDN] = state
+# LED 토글 라우트
+@app.route('/toggle_led/<int:led>', methods=['POST'])
+def toggle_led(led):
+    ledStates[led] = 1 if ledStates[led] == 0 else 0
     updateLeds()
     return redirect(url_for('index'))
 
