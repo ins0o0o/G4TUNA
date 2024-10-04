7955621d276d5316424cb25225011fa9ce3f9bbf
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-10-04 18:49:41 +0900

    Update sibal.py

--- week3/sibal.py
+++ week3/sibal.py
@@ -11,7 +11,6 @@ def send_json():
         'city': 'Seoul'
     }
     # JSON 형식으로 변환하여 응답
-    return jsonify(data)
-
+    return "sibal"
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
