13d55573295d3f0326ff8ccd463fd9cc1c2ca28a
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-10-04 18:47:31 +0900

    Create sibal.py

---
+++ week3/sibal.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/get-json', methods=['GET'])
+def send_json():
+    # 응답할 데이터 (Python 딕셔너리)
+    data = {
+        'name': 'John Doe',
+        'age': 30,
+        'city': 'Seoul'
+    }
+    # JSON 형식으로 변환하여 응답
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
