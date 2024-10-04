003802611cddfbf8fc163e05ebc26f676fe842ae
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-10-04 16:06:06 +0900

    Create test3.py

---
+++ weahter/test3.py
@@ -0,0 +1,27 @@
+import requests
+import xml.etree.ElementTree as ET
+
+url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst'
+params = {
+    'serviceKey': 'gAFYOesD02xHwlm93c35FiqgHKnqAJp6d0+jWA3aPcN6DAeVtK22eFtV8gA493BmO4azi7xqk9RY5KdKpeBvTA==',
+    'pageNo': '1',
+    'numOfRows': '1000',
+    'dataType': 'XML',
+    'base_date': '20241004',
+    'base_time': '0600',
+    'nx': '55',
+    'ny': '127'
+}
+
+response = requests.get(url, params=params)
+
+# XML 데이터 파싱
+root = ET.fromstring(response.content)
+
+# 현재 온도 찾기
+for item in root.iter('item'):
+    category = item.find('category').text
+    if category == 'T1H':  # T1H는 기온(온도)을 의미함
+        temperature = item.find('obsrValue').text
+        print(f"현재 온도: {temperature}°C")
+        break
