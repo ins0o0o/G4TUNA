129706f0ff083d9871f4f1c90895c2c3247ad472
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-10-09 02:43:35 +0900

    Update server.py

--- week5/server.py
+++ week5/server.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import ssl
 
 app = Flask(__name__)
-CORS(app)    # 모바일기기 에서 돌리려면 필요할 수도 있대
+CORS(app)    
 
 
 @app.route('/tlqkf', methods=['GET'])
@@ -11,4 +11,4 @@ def send_json():
     return "tlqkf"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')    #ssl 인증서 받아오고 염병 떨어야 될거 같은데
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc')   
