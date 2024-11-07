444af98454409533bdff4662eaceed27ffd678e2
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-11-07 12:06:36 +0900

    Update test6.py

--- weahter/test6.py
+++ weahter/test6.py
@@ -5,7 +5,7 @@ import urllib.parse
 url = 'http://apis.data.go.kr/1360000/LivingWthrIdxServiceV4/getUVIdxV4'
 queryParams = '?' + urllib.parse.urlencode({
     urllib.parse.quote_plus('ServiceKey'): 'gAFYOesD02xHwlm93c35FiqgHKnqAJp6d0+jWA3aPcN6DAeVtK22eFtV8gA493BmO4azi7xqk9RY5KdKpeBvTA==',
-    urllib.parse.quote_plus('areaNo'): '1100000000',  # 서울 지역 코드 (예시)
+    urllib.parse.quote_plus('areaNo'): '1121571000',  # 서울 지역 코드 (예시)
     urllib.parse.quote_plus('time'): '2024110718',   # 예시 시간: 2022년 07월 11일 18시
     urllib.parse.quote_plus('dataType'): 'XML'
 })
