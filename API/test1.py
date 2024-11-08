59585afb6631a8937636ccb6d111c2ca086113fc
ins0o0o <143942841+ins0o0o@users.noreply.github.com>
2024-11-08 11:03:18 +0900

    Update test1.py

--- API/test1.py
+++ API/test1.py
@@ -151,14 +151,14 @@ def get_calendar_events(calendar_id):
 calendar_id = input("\n캘린더 ID를 입력하세요: ")
 
 # 통합 실행
-print("날씨 정보:")
+
 get_weather_forecast()
-print("\n미세먼지 정보:")
+print("\n")
 get_dust_forecast()
-print("\n자외선 정보:")
+print("\n")
 get_uv_index()
 
 # Google Calendar 일정 조회
 
-print("\n일정 정보:")
+print("\n")
 get_calendar_events(calendar_id)
